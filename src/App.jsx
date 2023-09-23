9001c210fc188b7a4a5d04c633c6eec9297005f1
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-09-23 17:57:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import '../styles/Header.css'
+
+function App() {
+
+  return (
+    <main className='main-box'>
+      Demo for UI
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#ffffff',
+      'secondary': '#0f172a',
+      'tertiary': '#1e293b',
+      'accent': '#94a3b8'
+    }
+  },
+  plugins: [],
+}
